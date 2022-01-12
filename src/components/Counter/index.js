72e45edac36ad6bd037271051baae2500646c26f
ravigordon <rgordz460@outlook.com>
2022-01-12 12:00:49 +0000

    counter useReduction refactor

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,25 +1,59 @@
-import React, { useState } from "react";
+
+import React, { useState,useReducer } from "react";
+
+function countReducer(count,action){switch (action.type) {
+  case 'increment':
+  return count + 1;
+  case 'decrement':
+  return count - 1;
+  default:
+    throw new Error();
+}
+}
+
 
 function Counter() {
-  const [count, setCount] = useState(0);
-
-  function incrementCount() {
-    const updatedCount = count + 1;
-    setCount(updatedCount);
-  }
-
-  function decrementCount() {
-    const updatedCount = count - 1;
-    setCount(updatedCount);
-  }
-
-  return (
-    <div>
-      <button onClick={incrementCount}>+</button>
-      <p>{count}</p>
-      <button onClick={decrementCount}>-</button>
-    </div>
+const [count, dispatch] = useReducer(countReducer, 0);
+return(
+  <div>
+          Count: {count}
+          <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+          <button onClick={() => dispatch({type: 'increment'})}>+</button>
+  </div>
   );
 }
 
-export default Counter;
+ export default Counter;
+  
+ 
+//   const [count, setCount] = useState(0);
+
+//   function incrementCount() {
+//     const updatedCount = count + 1;
+//     setCount(updatedCount);
+//   }
+
+//   function decrementCount() {
+//     const updatedCount = count - 1;
+//     setCount(updatedCount);
+//   }
+
+//   return (
+//     
+//   );
+// }
+
+
+
+
+// const initialState = {count: 0};
+
+// function reducer(state, action) {
+//   ;
+//   }
+// }
+
+// function Counter() {
+
+//   
+// }
